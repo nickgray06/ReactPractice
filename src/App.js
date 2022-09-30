67c41a09fca99c9af7154636703af11992a8d811
nickgray06 <nickgray87@gmail.com>
2022-09-30 12:52:42 -0600

    JSX vs JS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  // return React.createElement(
+  //   "form",
+  //   {},
+  //   React.createElement("h1", {}, "Login"),
+  //   React.createElement("input", {
+  //     type: "text",
+  //     placeholder: "Name",
+  //     value: "",
+  //   }),
+  //   React.createElement("br", {}),
+  //   React.createElement("br", {}),
+  //   React.createElement("input", {
+  //     type: "password",
+  //     placeholder: "password",
+  //     value: "",
+  //   }),
+  //   React.createElement("br", {}),
+  //   React.createElement("br", {}),
+  //   React.createElement("button", { type: "submit" }, "Login")
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <h2>Login</h2>
+      <input type="text" placeholder="Name" />
+      <br />
+      <br />
+      <input type="password" placeholder="password" />
+      <br />
+      <br />
+      <input type="submit" nvalue="log" />
+    </form>
   );
 }
 
