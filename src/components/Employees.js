563f0058dfd266655e8dcf067270cc9e83f56ee6
nickgray06 <nickgray87@gmail.com>
2022-10-28 14:58:45 -0600

    refactor employees

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -5,18 +5,115 @@ import axios from "axios";
 
 export const Employees = () => {
   const [employees, setEmployee] = useState([]);
-  const addEmployee = () => {
-    setEmployee([
-      ...employees,
-      { empId: 6789, name: "Sam", designation: "TL" },
-    ]);
-  };
+  // const Employees = () => {
+  //   setEmployee(res.data);
+  // };
   useEffect(() => {
-    axios.get("employees.json").then((result) => setEmployee(result.data));
+    axios
+      .get("http://localhost:4000/employees")
+      .then((result) => setEmployee(result.data));
   }, []);
+  const [name, setName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [addFlag, setAddFlag] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState("");
+  const addEmployee = (e) => {
+    e.preventDefault();
+    setSuccess("");
+    if (name === "" || designation === "") {
+      setMessage("Please Enter Values");
+    } else {
+      setMessage("");
+      setAddFlag(false);
+      let newEmployee = { name: name, designation: designation };
+      axios.post("http://localhost:4000/employees", newEmployee).then((res) => {
+        setEmployee([...employees, res.data]);
+        setSuccess(`New employee has been added with the id ${res.data.id}`);
+      });
+      setName("");
+      setDesignation("");
+    }
+  };
+  const deleteEmployee = (empId) => {
+    setSuccess("");
+    let employeeId = parseInt(empId);
+    axios.delete("http://localhost:4000/employees" + employeeId).then((res) => {
+      axios.get("http://localhost:4000/employees").then((res) => {
+        setEmployee(res.data);
+      });
+    });
+  };
   return (
     <>
-      <Employee employees={employees} addEmployee={addEmployee} />
+      <table style={{ width: "60%" }} className="table table-bordered">
+        <thead className="thead-light">
+          <tr>
+            <th>EmpID</th>
+            <th>Name</th>
+            <th>Designation</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.length > 0 ? (
+            employees.map((employee) => {
+              return (
+                <tr key={employee.empId}>
+                  <td>{employee.empId}</td>
+                  <td>{employee.name}</td>
+                  <td>{employee.designation}</td>
+                  <td>
+                    <button onClick={() => deleteEmployee(employee.id)}>
+                      <i className="fa fa-trash"></i>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <div>No Data Found</div>
+          )}
+        </tbody>
+      </table>
+      <button onClick={() => setAddFlag(!addFlag)} className="btn btn-primary">
+        Add Employee
+      </button>{" "}
+      <br /> <br />
+      <div className="text-success">{success}</div>
+      {addFlag ? (
+        <form>
+          EmpName:{" "}
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+              setMessage("");
+            }}
+          />{" "}
+          <br /> <br />
+          Designation:{" "}
+          <input
+            type="text"
+            value={designation}
+            onChange={(e) => {
+              setDesignation(e.target.value);
+              setMessage("");
+            }}
+          />{" "}
+          <br /> <br />
+          <button className="btn btn-primary" onClick={addEmployee}>
+            Add
+          </button>
+          <div className="text-danger">{message}</div>
+        </form>
+      ) : null}
+      {/* <Employee
+        employees={employees}
+        addEmployee={addEmployee}
+        deleteEmployee={deleteEmployee}
+      /> */}
     </>
   );
 };
